c406cf6a1236a1b5d5519b1c347e3eaf92aa76ea
youngbin pyo <youngbinpyo@youngbins-MacBook-Pro.local>
2024-11-09 12:16:25 +0200

    part 1.4

--- exercise1/part1/src/App.jsx
+++ exercise1/part1/src/App.jsx
@@ -36,19 +36,21 @@ const Total = ({ parts }) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
+  const parts = [
+  {
       name: 'Fundamentals of React',
       exercises: 10
-  }
-  const part2 = {
+  },
+  {
       name: 'Using props to pass data',
       exercises: 7
-  }
-  const part3 = {
+  },
+  {
       name: 'State of a component',
       exercises: 14
   }
-  
+]
+
   return (
     <div>
       <Header course={course} />
