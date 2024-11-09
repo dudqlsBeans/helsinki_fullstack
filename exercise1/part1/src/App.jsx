9542ed313eea148950e28e73001c2b1e650471ba
youngbin pyo <youngbinpyo@youngbins-MacBook-Pro.local>
2024-11-09 22:50:15 +0200

    exercise 1.9

--- exercise1/part1/src/App.jsx
+++ exercise1/part1/src/App.jsx
@@ -7,14 +7,22 @@ const Button = (props) => (
   </button>
 )
 
+const Statistics = (props) => {
+  return (
+    <div>
+      <p>{props.name} {props.count}</p>
+    </div>
+  )
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
   const all = good + neutral + bad
-  const average = (good - bad) / all
-  const positive = good / all
+  const average = all > 0 ? ((good - bad) / all) : 0
+  const positive = all > 0 ? (good / all) : 0
 
   const setToGood = newValue => {
     console.log('good value now', newValue)
@@ -38,13 +46,19 @@ const App = () => {
       <Button handleClick={() => setToGood(good + 1)} text="good" />
       <Button handleClick={() => setToNeutral(neutral + 1)} text="neutral" />
       <Button handleClick={() => setToBad(bad + 1)} text="bad" />
-      <h1>statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {all}</p>
-      <p>average {average}</p>
-      <p>positive {positive}</p>
+      <h1>Statistics</h1>
+      {all > 0 ? (
+        <div>
+          <Statistics name="good" count={good} />
+          <Statistics name="neutral" count={neutral} />
+          <Statistics name="bad" count={bad} />
+          <Statistics name="all" count={all} />
+          <Statistics name="average" count={average} />
+          <Statistics name="positive" count={positive} />
+        </div>
+      ) : (
+        <p>No feedback given</p>
+      )}
     </div>
   )
 }
