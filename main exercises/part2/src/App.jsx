2e32b125b27dbc3538152b880f1456165cb9f216
youngbin pyo <youngbinpyo@youngbins-MacBook-Pro.local>
2025-06-25 15:04:06 +0900

    feat(App): Added code for exercise 2.1

--- main exercises/part2/src/App.jsx
+++ main exercises/part2/src/App.jsx
@@ -1,16 +1,60 @@
-import Note from './components/Note'
-
-const App = (notes) => {
+const Course = ({course}) => {
   return (
     <div>
-      <h1>Notes</h1>
-      <ul>
-        {notes.map(note => 
-          <Note key={note.id} note={note}/>
-        )}
-      </ul>
+      <Header name={course.name} />
+      <Content parts={course.parts} />
     </div>
   )
+}
+
+const Header = ({ name }) => {
+  return <h2>{name}</h2>
+}
+
+const Content = ({ parts }) => {
+  console.log("Parts received in Content:", parts)
+  return (
+    <div>
+      {parts.map(part => (
+        <Part key={part.id} part={part} />
+      ))}
+    </div>
+  )
+}
+
+const Part = ({ part }) => {
+  return (
+    <p>
+      {part.name} {part.exercises}
+    </p>
+  )
+}
+
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+  console.log("working here")
+  return <Course course={course} />
 }
 
 export default App
